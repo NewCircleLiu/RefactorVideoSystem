bdbf6c0869d911310436f7043002e5cbf867bc4d
Zhengjun-Du <duzjqhu@aliyun.com>
2017-09-29 23:31:53 +0800

    修改了前端和后端布局

--- GoodVideoSystem/GoodVideoSystem/Resources/js/utility/welcome.js
+++ GoodVideoSystem/GoodVideoSystem/Resources/js/utility/welcome.js
@@ -11,11 +11,11 @@
 })
 
 //设定倒数时间（秒）
-var t = 3;
+var t = 4;
 
 //显示倒数秒数
 function showTime() {
-    jQuery("#info").text("将在" + t + "秒后进入系统");
+    jQuery("#info").text("欢迎您访问古驿平安串珠视频系统！正在准备，稍后将进入系统...");
     if (t == 1) {
         clearTimeout(showTime);
         location.href = '/User/Home';
